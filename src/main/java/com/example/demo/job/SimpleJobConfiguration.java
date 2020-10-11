41a34cf0e6ff4b5c0db59a20fc6d3b841e01461e
chiyoon <cysong.dev@gmail.com>
2020-10-11 16:18:41 +0900

    [2020-10-11 SCY] 스프링배치 샘플 코드 * datasource 설정 정보 수정 * Step 실행 * 애플리케이션 실행시 parameter 전달

--- src/main/java/com/example/demo/job/SimpleJobConfiguration.java
+++ src/main/java/com/example/demo/job/SimpleJobConfiguration.java
@@ -5,8 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -24,19 +26,30 @@ public class SimpleJobConfiguration {
          * Job : 배치 작업 단위
          */
         return jobBuilderFactory.get("simpleJob")
-                .start(simpleStep1())
+                .start(simpleStep1(null))
+                .next(simpleStep2(null))
                 .build();
     }
 
     @Bean
-    public Step simpleStep1() {
+    @JobScope
+    public Step simpleStep1(@Value("#{jobParameters[requestDate]}") String requestDate) {
         return stepBuilderFactory.get("simpleStep1") // simpleStep1 이란 이름의 Batch Step 생성.
                 .tasklet((contribution, chunkContext) -> {
-                    /**
-                     * Step 안에서 수행될 기능 명시
-                     * Tasklet은 Step안에서 단일로 수행될 커스텀한 기능들을 선언할때 사용
-                     */
                     log.info(">>>>> This is Step1");
+                    log.info(">>>>> requestDate = {}", requestDate);
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step simpleStep2(@Value("#{jobParameters[requestDate]}") String requestDate) {
+        return stepBuilderFactory.get("simpleStep2") // simpleStep1 이란 이름의 Batch Step 생성.
+                .tasklet((contribution, chunkContext) -> {
+                    log.info(">>>>> This is Step2");
+                    log.info(">>>>> requestDate = {}", requestDate);
                     return RepeatStatus.FINISHED;
                 })
                 .build();
